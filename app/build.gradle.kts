51c83ddb1067de07368dd6b822d2874c94ec6481
Sebastiano Poggi <poggos@gmail.com>
2021-05-02 17:02:34 +0200

    Clean up gradle script

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     id("dagger.hilt.android.plugin")
 }
 
+val composeVersion by extra { "1.0.0-beta05" }
+
 android {
     compileSdk = 30
     buildToolsVersion = "30.0.3"
@@ -38,7 +40,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.0-beta01"
+        kotlinCompilerExtensionVersion = composeVersion
     }
 }
 
@@ -47,18 +49,19 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.3.0-alpha07")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.compose.foundation:foundation:1.0.0-beta05")
-    implementation("androidx.compose.material:material-icons-extended:1.0.0-beta05")
-    implementation("androidx.compose.material:material:1.0.0-beta05")
-    implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta05")
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-beta05")
-    implementation("androidx.compose.ui:ui:1.0.0-beta05")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha01")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha01")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.4.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha01")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha04")
     implementation("androidx.navigation:navigation-compose:1.0.0-alpha10")
-    implementation("com.jakewharton.timber:timber:4.7.1")
     implementation("com.google.dagger:hilt-android:$hiltVersion")
+    implementation("com.jakewharton.timber:timber:4.7.1")
+
     kapt("com.google.dagger:hilt-compiler:$hiltVersion")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha01")
 }
