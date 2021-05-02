59f7770d75cfa5e2e389af4aa39c115d3378b274
Sebastiano Poggi <poggos@gmail.com>
2021-05-02 20:00:42 +0200

    Clean up code

--- app/src/main/java/dev/sebastiano/bundel/NotificationsListScreen.kt
+++ app/src/main/java/dev/sebastiano/bundel/NotificationsListScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
