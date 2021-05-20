f39e70130d51fd6de88bd12085abd0dd75446257
Ivan Morgillo <imorgillo@gmail.com>
2021-05-20 16:25:20 +0200

    chore: killed jCenter

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,14 @@ buildscript {
         gradlePluginPortal()
         google()
         mavenCentral()
-        jcenter()
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("com.android.tools.build:gradle:7.0.0-alpha15")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
-        classpath ("com.google.gms:google-services:4.3.8")
-        classpath ("com.google.firebase:firebase-crashlytics-gradle:2.6.1")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.6.1")
+        classpath("com.google.gms:google-services:4.3.8")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
     }
 }
 
@@ -26,6 +25,5 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        jcenter()
     }
 }
