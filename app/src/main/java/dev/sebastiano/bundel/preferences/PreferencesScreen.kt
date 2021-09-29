5d52bd52da994f0e6f31902bb63974e892f33a83
Sebastiano Poggi <poggos@gmail.com>
2021-09-29 20:36:44 +0200

    Fix static analysis warnings

--- app/src/main/java/dev/sebastiano/bundel/preferences/PreferencesScreen.kt
+++ app/src/main/java/dev/sebastiano/bundel/preferences/PreferencesScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
