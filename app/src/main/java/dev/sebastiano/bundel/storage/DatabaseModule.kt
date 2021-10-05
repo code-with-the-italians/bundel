a75e37e111780fa11da1e3529a38ace1285d542d
ZianeA <wit_wit90@hotmail.com>
2021-10-06 00:31:44 +0100

    Optimize DatabaseModule DI setup

--- app/src/main/java/dev/sebastiano/bundel/storage/DatabaseModule.kt
+++ app/src/main/java/dev/sebastiano/bundel/storage/DatabaseModule.kt
@@ -2,22 +2,27 @@ package dev.sebastiano.bundel.storage
 
 import android.app.Application
 import androidx.room.Room
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.sebastiano.bundel.storage.migrations.Migration1to2
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal class DatabaseModule {
+internal abstract class DatabaseModule {
 
-    @Provides
-    fun provideImagesStorage(application: Application): ImagesStorage = DiskImagesStorage(application)
+    @Binds
+    abstract fun provideImagesStorage(diskImagesStorage: DiskImagesStorage): ImagesStorage
 
-    @Provides
-    fun provideDatabase(application: Application): RobertoDatabase =
-        Room.databaseBuilder(application, RobertoDatabase::class.java, "roberto")
-            .addMigrations(Migration1to2)
-            .build()
+    companion object {
+        @Singleton
+        @Provides
+        fun provideDatabase(application: Application): RobertoDatabase =
+            Room.databaseBuilder(application, RobertoDatabase::class.java, "roberto")
+                .addMigrations(Migration1to2)
+                .build()
+    }
 }
